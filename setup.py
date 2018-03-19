9ea48c35d6ef3001d6f6e3de58b276dfaf553a40
Amador Pahim <apahim@redhat.com>
2018-03-19 11:39:31 +0100

    Updating dependencies
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='avocado-qemu',
       url='http://avocado-framework.github.io/',
       packages=['avocado_qemu'],
       include_package_data=True,
-      install_requires=['avocado-framework']
+      install_requires=['avocado-framework>=59.0', 'aexpect']
       )
